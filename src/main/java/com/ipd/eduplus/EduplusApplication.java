1f37a8a056a3e3cf36ea41f12cfdbe7f2e28d872
Fatima Arame Sarr <fatima9arame@gmail.com>
2026-06-10 14:54:25 +0000

    feat: initialisation projet EduPlus

---
+++ src/main/java/com/ipd/eduplus/EduplusApplication.java
@@ -0,0 +1,13 @@
+package com.ipd.eduplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipd/eduplus/EduplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipd.eduplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
