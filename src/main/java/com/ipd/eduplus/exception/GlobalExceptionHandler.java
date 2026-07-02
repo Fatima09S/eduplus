9a515eeb1dafc7c8413216c2edee3895e1374764
Ibraima Dione <ibrahima.dione8@unchk.edu.sn>
2026-07-02 23:49:47 +0000

    feat(exception): gestion de ResourceNotFoundException avec GlobalExceptionHandler

--- src/main/java/com/ipd/eduplus/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ipd/eduplus/exception/GlobalExceptionHandler.java
@@ -1,8 +1,30 @@
 package com.ipd.eduplus.exception;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(
+            ResourceNotFoundException ex,
+            HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(error);
+    }
 
 }
