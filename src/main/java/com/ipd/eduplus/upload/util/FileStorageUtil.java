d6ccd6c20236ed35d85b818b934f92740e62e878
Ibraima Dione <ibrahima.dione8@unchk.edu.sn>
2026-07-09 23:46:41 +0000

    feat(upload): ajout de la génération de noms de fichiers uniques

--- src/main/java/com/ipd/eduplus/upload/util/FileStorageUtil.java
+++ src/main/java/com/ipd/eduplus/upload/util/FileStorageUtil.java
@@ -1,4 +1,23 @@
 package com.ipd.eduplus.upload.util;
 
+import java.util.UUID;
+
 public class FileStorageUtil {
+
+    private FileStorageUtil() {
+    }
+
+    public static String generateFileName(String originalFileName) {
+
+        String extension = "";
+
+        int index = originalFileName.lastIndexOf(".");
+
+        if (index > 0) {
+            extension = originalFileName.substring(index);
+        }
+
+        return UUID.randomUUID() + extension;
+    }
+
 }
