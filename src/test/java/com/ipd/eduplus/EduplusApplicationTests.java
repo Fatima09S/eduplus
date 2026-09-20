bafb38525820752c25a278ec4b52ccca8ec598ed
Fatima Arame Sarr <fatima9arame@gmail.com>
2026-09-20 10:20:29 -0400

    fix: configurer les tests CI avec H2

--- src/test/java/com/ipd/eduplus/EduplusApplicationTests.java
+++ src/test/java/com/ipd/eduplus/EduplusApplicationTests.java
@@ -2,12 +2,23 @@ package com.ipd.eduplus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"app.jwt.secret=test-secret-for-eduplus-ci-only",
+		"app.jwt.expiration=86400000",
+		"app.jwt.access-token-expiration=900000",
+		"app.jwt.refresh-token-expiration=604800000",
+		"spring.datasource.url=jdbc:h2:mem:eduplus_test",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.sql.init.mode=never"
+})
+@ActiveProfiles("test")
 class EduplusApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
